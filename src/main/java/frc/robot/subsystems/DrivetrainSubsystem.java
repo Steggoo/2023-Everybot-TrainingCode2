0d16936b5e783cd6ba4b94d21c5296e0f58403b3
Steggo 4467 <114831754+Steggoo@users.noreply.github.com>
2023-06-28 21:36:32 -0400

    Feat: Created new drivetrain subsystem

---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+
+    public DrivetrainSubsystem() {
+        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+        //       in the constructor or in the robot coordination class, such as RobotContainer.
+        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
+        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+    }
+}
+
